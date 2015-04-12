be576bf6c432893074a1b49c397f0564e2993817
Goran Mekić <meka@lugons.org>
2015-04-12 19:25:38 +0200

    Create default superuser

--- bin/run.sh
+++ bin/run.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-mkdir /tilda_center/static
-mkdir /tilda_center/media
+mkdir /tilda_center/static || true
+mkdir /tilda_center/media || true
 python /app/manage.py collectstatic --noinput
-python /app/manage.py migrate
+python /app/manage.py createdb --noinput || true
+python /app/manage.py migrate --noinput
+python /app/manage.py loaddata /app/user.json
 uwsgi --ini /app/uwsgi.ini
