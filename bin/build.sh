2ff3a04e2a686b2833f98137bd391c56ba968342
Goran Mekić <meka@lugons.org>
2015-04-12 18:40:28 +0200

    Keep postgresql library

--- bin/build.sh
+++ bin/build.sh
@@ -2,19 +2,22 @@
 
 set -e
 
-SYSTEM_PACKAGES="\
+PACKAGES="\
+    libpq5"
+
+TEMP_PACKAGES="\
     postgresql-server-dev-9.4 \
     python-pip \
     python-dev"
 
 mv /app/sources.list /etc/apt
 apt-get update
-apt-get install -y $SYSTEM_PACKAGES
+apt-get install -y ${PACKAGES} ${TEMP_PACKAGES}
 
 cd /app
 pip install -r requirements.txt
 rm -f Dockerfile Vagrantfile build.sh
 
-apt-get purge -y ${SYSTEM_PACKAGES}
+apt-get purge -y ${TEMP_PACKAGES}
 apt-get autoremove -y --purge
 apt-get clean
