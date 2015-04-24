a68589ddf99a2a5d235eff90e0753353ddec45a0
Goran Mekić <meka@lugons.org>
2015-04-24 15:03:59 +0200

    Add Nova theme

--- bin/runserver.sh
+++ bin/runserver.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sudo docker run -it -p 8000:8000 -v /vagrant/projects/tilda:/app --rm --link postgresql:postgresql mekanix/tilda_center:latest python /app/manage.py runserver 0.0.0.0:8000
+arguments="$@"
+if [ -z $arguments ]; then
+    arguments="python /app/manage.py runserver 0.0.0.0:8000"
+fi
+
+sudo docker run -it -p 8000:8000 -v /vagrant/projects/tilda:/app --rm --link postgresql:postgresql mekanix/tilda_center:latest $arguments
