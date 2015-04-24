b0f1efcad4452e0febc6c2f977e648b1e95435f7
Goran Mekić <meka@lugons.org>
2015-04-24 14:28:49 +0200

    Fix runserver script

--- bin/runserver.sh
+++ bin/runserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run -p 8000:8000 -v /vagrant/projects/tilda:/app --rm --link postgresql:postgresql mekanix/tilda_center:latest
+sudo docker run -it -p 8000:8000 -v /vagrant/projects/tilda:/app --rm --link postgresql:postgresql mekanix/tilda_center:latest python /app/manage.py runserver 0.0.0.0:8000
