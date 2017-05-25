7220d027e80bda5962fe721951c0bf45e3398a74
Jelena Dokic <jelena.dpk@gmail.com>
2017-05-25 21:17:37 +0200

    Add frontend project

---
+++ frontend/src/reducers.js
@@ -0,0 +1,20 @@
+import { combineReducers } from 'redux';
+import login from './pages/login/reducers';
+import settings from './templates/default/reducers';
+import theme from './containers/reducers';
+import { backend, notifications } from './containers/app/reducers';
+
+
+const reducers = {
+  backend,
+  login,
+  notifications,
+  theme,
+};
+
+settings.forEach(reducer => { reducers[reducer.name] = reducer; });
+
+
+const rootReducer = combineReducers(reducers);
+
+export default rootReducer;
