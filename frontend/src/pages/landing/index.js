fe4c6972da5ae259acc462d01dc5f20b6d329f7d
Maja Mekić <ahrimana@tilda.center>
2017-06-15 23:43:28 +0200

    landing to land

--- frontend/src/pages/landing/index.js
+++ frontend/src/pages/landing/index.js
@@ -11,7 +11,7 @@ class Landing extends Component {
     return (
       <Template>
         <Paper style={styles.root}>
-          Landing
+          Land
         </Paper>
       </Template>
     );
