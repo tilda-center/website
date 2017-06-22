dc659147ff4a975bf936e2ba59f5624de97a9195
Maja Mekić <ahrimana@tilda.center>
2017-06-23 00:56:38 +0200

    landing izmenjen i stilovi

--- frontend/src/pages/landing/index.js
+++ frontend/src/pages/landing/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Paper from 'material-ui/Paper';
 import getStyles from './styles';
+import AppBar from 'material-ui/AppBar';
 import Template from '../../templates/default';
 
 
@@ -9,11 +10,14 @@ class Landing extends Component {
   render() {
     const styles = getStyles(this.context.muiTheme);
     return (
+    <div style={styles.firstpage}>
+      <AppBar title="Tilda Center" />
       <Template>
         <Paper style={styles.root}>
           Land
         </Paper>
       </Template>
+    </div>
     );
   }
 }
