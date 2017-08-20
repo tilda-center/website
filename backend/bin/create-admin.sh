b502ad80c5fec4a5464c883a4184466a2094e5e6
Goran Mekić <meka@tilda.center>
2017-08-20 13:31:50 +0200

    Initialize DB

--- backend/bin/create-admin.sh
+++ backend/bin/create-admin.sh
@@ -4,10 +4,7 @@ export FLASK_CONFIG="dev"
 BIN_DIR=`dirname $0`
 PROJECT_ROOT=`readlink -f "${BIN_DIR}/.."`
 
-echo "Tilda Backend"
-echo "==============="
+echo "Creating admin"
 . ~/.virtualenvs/${VIRTUALENV}/bin/activate
 cd ${PROJECT_ROOT}
-pip install -U -r requirements.txt
-./manage.py db migrate
-./manage.py runserver
+./manage.py create_admin -e admin@example.com -p Sekrit
--- backend/manage.py
+++ backend/manage.py
@@ -13,17 +13,16 @@ config_name = os.getenv('FLASK_CONFIG') or 'default'
 app = Flask(__name__)
 app.config.from_object(configs[config_name])
 tilda_center = TildaCenter(app)
+
 manager = Manager(tilda_center.app)
-manager.add_command(
-    "runserver",
-    Server(
-        host="0.0.0.0",
-        use_reloader=True,
-        use_debugger=True
-    )
-)
 manager.add_command('create_admin', CreateAdminCommand)
 manager.add_command('db', tilda_center.db.manager)
+manager.add_command('runserver', Server(
+        host='0.0.0.0',
+        use_reloader=True,
+        use_debugger=True,
+    )
+)
 
 @app.route('/')
 def index():
