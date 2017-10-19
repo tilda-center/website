ebe4709433c13bdf7a4e847f276ba110ca906453
Goran Mekić <meka@tilda.center>
2017-10-19 11:49:47 +0200

    Edit title

--- frontend/src/pages/event/index.js
+++ frontend/src/pages/event/index.js
@@ -6,6 +6,7 @@ import RichTextEditor from 'react-rte';
 import Moment from 'react-moment';
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 import Template from '../../templates/default';
 import { isLoggedIn } from '../../utils';
 import actions from './actions';
@@ -63,10 +64,32 @@ class Event extends React.Component {
         this.setState({ showEditor: false });
       }
     };
+
+    this.editTitle = () => {
+      if (isLoggedIn()) {
+        this.setState({ showTitleEditor: true });
+      }
+    };
+
+    this.hideTitleEditor = () => {
+      this.setState({ showTitleEditor: false });
+    };
+
+    this.saveTitle = () => {
+      const fields = { title: this.state.title };
+      this.props.set(this.props.match.params.eventId, fields);
+      this.props.get(this.props.match.params.eventId);
+      this.setState({ showTitleEditor: false });
+    };
+
+    this.handleTitleChange = (event, title) => {
+      this.setState({ title });
+    };
   }
 
   state = {
     showEditor: false,
+    showTitleEditor: false,
     value: RichTextEditor.createEmptyValue(),
   }
 
@@ -77,6 +100,7 @@ class Event extends React.Component {
   componentWillReceiveProps(nextProps) {
     if (nextProps.event) {
       this.setState({
+        title: nextProps.event.title,
         value: RichTextEditor.createValueFromString(
           nextProps.event.markdown,
           'markdown',
@@ -86,20 +110,35 @@ class Event extends React.Component {
   }
 
   render() {
-    const markdownWidget = this.state.showEditor
+    const markdownComponent = this.state.showEditor
+                            ? (
+                              <div>
+                                <RichTextEditor
+                                  value={this.state.value}
+                                  onChange={this.handleEditorChange}
+                                />
+                                <RaisedButton label="save" primary onClick={this.saveMarkdown} />
+                                <RaisedButton label="cancel" onClick={this.hideMarkdownEditor} />
+                              </div>
+                            ) : (
+                             <ReactMarkdown
+                               source={this.props.event.markdown}
+                             />
+                            );
+    const titleComponent = this.state.showTitleEditor
                          ? (
                            <div>
-                             <RichTextEditor
-                               value={this.state.value}
-                               onChange={this.handleEditorChange}
+                             <TextField
+                               autoFocus
+                               name="title"
+                               defaultValue={this.props.event.title}
+                               onChange={this.handleTitleChange}
                              />
-                             <RaisedButton label="save" primary onClick={this.saveMarkdown} />
-                             <RaisedButton label="cancel" onClick={this.hideMarkdownEditor} />
+                             <RaisedButton label="save" primary onClick={this.saveTitle} />
+                             <RaisedButton label="cancel" onClick={this.hideTitleEditor} />
                            </div>
                          ) : (
-                          <ReactMarkdown
-                            source={this.props.event.markdown}
-                          />
+                           <h1 onClick={this.editTitle}>{this.props.event.title}</h1>
                          );
     const content = this.props.eventStatus === 'error'
                   ? (
@@ -107,15 +146,15 @@ class Event extends React.Component {
                   ) : (
                     <div>
                       <div style={styles.meta}>
-                        <h1>{this.props.event.title}</h1>
                         <div>
+                          {titleComponent}
                           <Moment interval={0} format="DD.MM.YYYY HH:mm">
                             {this.props.event.date}
                           </Moment>
                         </div>
                       </div>
                       <div onClick={this.showMarkdownEditor}>
-                        {markdownWidget}
+                        {markdownComponent}
                       </div>
                     </div>
                   );
