fbe7ee867baaade2154e9c7cffecb0619ff908c6
Rajko Dervisevic <rajkod@hotmail.com>
2019-01-26 16:45:37 +0100

    About mail

--- services/backend/tilda/api/namespaces.py
+++ services/backend/tilda/api/namespaces.py
@@ -4,5 +4,6 @@ ns_auth = Namespace('auth', description='Auth operations')
 ns_blog = Namespace('blog', description='Blog operations')
 ns_me = Namespace('me', description='Me operations')
 ns_user = Namespace('users', description='User operations')
+ns_about = Namespace('about', description='About Page')
 
-namespaces = [ns_auth, ns_blog, ns_me, ns_user]
+namespaces = [ns_auth, ns_blog, ns_me, ns_user, ns_about]
