3a0cb9f429b91140eba1bb29ec419e246a61d462
Goran Mekić <meka@tilda.center>
2020-07-26 23:12:17 +0200

    Improve publish

--- bin/publish.sh
+++ bin/publish.sh
@@ -10,28 +10,8 @@ export frontend_app_name=${app_name}
 PASSWORD=`cat ${PROJECT_ROOT}/publish_password.txt`
 
 cd "${PROJECT_ROOT}"
-rm -rf ${PROJECT_ROOT}/build/*
-cp -r "${PROJECT_ROOT}/services/backend/${backend_app_name}/static" "${PROJECT_ROOT}/build/"
-cp -r ${PROJECT_ROOT}/services/frontend/build/* "${PROJECT_ROOT}/build/"
-rsync -P -avcl --delete-after build/ med.meka.rs:/usr/cbsd/jails-data/nginx-data/usr/local/www/med.meka.rs/
-rsync \
-  -P \
-  -avcl \
-  --delete-after \
-  --exclude=.git* \
-  --exclude=__pycache__ \
-  --exclude=Makefile \
-  --exclude=ansible \
-  --exclude=build \
-  --exclude=cbsd.conf \
-  --exclude=local_config.py \
-  --exclude=media \
-  --exclude=*.db \
-  --exclude=*.mk \
-  --exclude=requirements.yml \
-  --exclude=static \
-  --exclude=templates \
-  --exclude=tests \
-  services/backend/ \
-  med.meka.rs:/usr/cbsd/jails-data/med-data/usr/home/deploy/repos/med/
-"${BIN_DIR}/publish" "${PASSWORD}"
+rm -rf ${PROJECT_ROOT}/build
+mkdir ${PROJECT_ROOT}/build
+cp -r "services/backend/${backend_app_name}/static" "build/"
+cp -r services/frontend/build/* "build/"
+rsync -P -avcl --delete-after build/ chubby:/usr/cbsd/jails-data/nginx-data/usr/local/www/new.tilda.center/
